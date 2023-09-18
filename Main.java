2c7b6026bc5f7baf09c04dd5bf24764913605589
v187nce <134378283+v187nce@users.noreply.github.com>
2023-09-18 10:36:29 +0000

    created main class

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main{
+    public static void main(String[] args){
+
+    }
+
+    public void filmAusgeben(int zahl){
+        switch (zahl){
+            case 1: {
+                
+            }
+            case 2: {
+
+            }
+            case 3: {
+
+            }
+        }
+    }
+}
