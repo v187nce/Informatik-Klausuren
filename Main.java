94a7dd265892200d927c6c84730a5555438eed0a
JK <113830349+Juliorocktect@users.noreply.github.com>
2023-10-18 07:04:49 +0000

    verbesserung

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main{
     public static void main(String[] args){
         Agentur agentur1 = new Agentur();
         agentur1.neuerStatist("name", 'm');
+        agentur1.neuerStatist("name2",'w');
+        System.out.println(agentur1.mannOderFrau('w').element().getGeschlecht());
         
     }
 }
